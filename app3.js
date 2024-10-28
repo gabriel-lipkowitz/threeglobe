6e2dac31abf28d672025d613cdaadf3c32f47064
Gabriel Lipkowitz <gel@Gabriels-MacBook-Pro-133.local>
2024-10-28 15:53:00 -0700

    first commit

--- app3.js
+++ app3.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from './GLTFLoader.js';
 
 const models = {
-  stanford: './models/stanford.glb',
+  stanford: './dist/models/stanford.glb',
   // add other models
 };
 const modelCache = {};
@@ -17,6 +17,7 @@ async function loadModel(url) {
 async function preloadModels() {
   const loadPromises = Object.entries(models).map(([label, url]) =>
     loadModel(url).then(model => {
+        model.scale.set(10,10,10)
       modelCache[label] = model;
     })
   );
@@ -24,16 +25,19 @@ async function preloadModels() {
   await Promise.all(loadPromises);
   console.log("All models loaded!");
   initializeGlobe();  // Initialize the globe after all models are loaded
+  
 }
 
 function initializeGlobe() {
   const globe = new Globe()
+  (document.getElementById('globeViz'))
     .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+    .objectsData([
+        { lat: 40.3430, lng: -74.6514, label: 'stanford' }])
     .objectThreeObject(data => modelCache[data.label].clone())
     .objectLabel(obj => `<div>${obj.label}</div>`);
-    (document.getElementById('globeViz'));
 //   document.getElementById('globeViz').appendChild(globe.renderer().domElement);
-  globe.renderer().render(globe.scene(), globe.camera());
+//   globe.renderer().render(globe.scene(), globe.camera());
 }
 
 // Start preloading and then initialize the globe

2c5263b51275242290e5a240ac639427afc2ac10
Gabriel Lipkowitz <gel@Gabriels-MacBook-Pro-133.local>
2024-11-17 16:29:20 +0900

    models

--- app3.js
+++ app3.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from './GLTFLoader.js';
 
 const models = {
-  'stanford': './models/stanford.glb',
+  'stanford': './stanford.glb',
   // add other models
 };
 const modelCache = {};
